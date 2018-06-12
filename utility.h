6c720965f75c3df79330f158faa4c32a120a48c9
Victor Ordu <victorordu@outlook.com>
2018-06-12 19:45:09 +0100

    Submit assignment

---
+++ utility.h
@@ -0,0 +1,6 @@
+// Function declarations
+#include <string>
+
+std::string ReadStringFromConsole();
+
+std::string DescribePersonAsSomething(const std::string& person, const std::string& adjective);
